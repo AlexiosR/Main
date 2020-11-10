22f9d71b450f02e8d2f400a27c2009afeffebbf7
AlexiosR <alexey.ratov@gmail.com>
2020-11-10 20:10:46 +0200

    ахахахах пару сек назад

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,11 @@
-console.log('Hello World')
+ "use strict"
+        let imya ="John";
+        let vik = true;
+        let numCard = 25;
+        let maksPrice = 1000200030004000500060007000n;
+        let seks;
+        let misto = null;
+
+        console.log(
+            `${"string"} - ${imya}, ${"boolean"} - ${vik}, ${"number"} - ${numCard}, `
+            + 'bigint - ' + maxPrice + ', "undefined" - ' + sex + ', ' + "'null'" + ' - ' + city);
