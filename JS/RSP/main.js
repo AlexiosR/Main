7dbe094a7c2fb8a981bb945a207b8bd89b9f323a
AlexiosR <alexey.ratov@gmail.com>
2020-12-01 20:08:50 +0200

    Game

--- JS/RSP/main.js
+++ JS/RSP/main.js
@@ -2,49 +2,49 @@
 
 
 
-let rock = "rock";
-let scissors = "scissors";
-let paper = "paper";
+let rock = "Rock";
+let scissors = "Scissors";
+let paper = "Paper";
 
-let aboutRock = rock.toLowerCase();
-let aboutScissors = scissors.toLowerCase();
-let aboutPaper = paper.toLowerCase();
+let Rock = rock.toLowerCase();
+let Scissors = scissors.toLowerCase();
+let Paper = paper.toLowerCase();
 
 alert("Welcome to the Rock vs Scissors vs Paper");  
 
-let firstplayer = prompt('Pl_1 Write: Rock or Scissors or Paper', '');
-let secondplayer = prompt('Pl_2 Write: Rock or Scissors or Paper', '');
+let firstplayer = prompt('First player write: Rock or Scissors or Paper', '');
+let secondplayer = prompt('Second player write: Rock or Scissors or Paper', '');
 
 if ((firstplayer == rock) && (secondplayer == rock)
      || (firstplayer == scissors) && (secondplayer == scissors)
          || (firstplayer == paper) && (secondplayer == paper)){
-    alert('Draw,restart page press "F5" ')
+    alert('Draw');
 }else
 {
-    if ((firstplayer == paper) && (secondplayer == rock) )
-    {
-        alert('First Player Win, Paper > Rock');
-    }else{ if ((firstplayer == rock) && (secondplayer == scissors) )
-        {
-            alert('First Player Win, Rock > Scissors');
-        }else{ if ((firstplayer == scissors) && (secondplayer == paper) )
-            {
-                alert('пFirst Player Win, Scissors > Paper');
-            }else{ if ((secondplayer == paper) && (firstplayer == rock) )
-                {
-                    alert('Second Player Win, Paper > Rock');
-                }else { if ((secondplayer == rock) && (firstplayer == scissors) )
-                    {
-                        alert('Second Player Win, Rock > Scissors');
-                    }else { if ((secondplayer == scissors) && (firstplayer == paper) )
-                        {
-                            alert('Second Player Win, Scissors > Paper');
-                        }else{
-                            alert("418 I’m a teapot");}
-            }
-        }
-    }
+if ((firstplayer == paper) && (secondplayer == rock) )
+{
+    alert('First Player Win, Paper > Rock');
+}else{ if ((firstplayer == rock) && (secondplayer == scissors) )
+{
+    alert('First Player Win, Rock > Scissors')
+}else{ if ((firstplayer == scissors) && (secondplayer == paper) )
+{
+    alert('пFirst Player Win, Scissors > Paper')
+}else{ if ((secondplayer == paper) && (firstplayer == rock) )
+{
+    alert('Second Player Win, Paper > Rock');
+}else { if ((secondplayer == rock) && (firstplayer == scissors) )
+{
+    alert('Second Player Win, Rock > Scissors')
+}else { if ((secondplayer == scissors) && (firstplayer == paper) )
+{
+    alert('Second Player Win, Scissors > Paper');
+}else{
+    alert("Error");}
 }
 }
 }
-alert("418 I’m a teapot ")
+}
+}
+}
+alert("Restart page - press F5")
